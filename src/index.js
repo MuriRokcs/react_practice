3d28b3d1dceed18a35b936517bea5436b94e2908
MuriRokcs <32195775+MuriRokcs@users.noreply.github.com>
2021-04-11 15:10:57 +0200

    event basics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// CSS
+import "./index.css";
+const books = [
+  {
+    id: 1,
+    author: "Amelia Hepworth",
+    title: "I Love You to the Moon and Back",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+  },
+  {
+    id: 2,
+    author: "Mark Tappan",
+    title: "My Dog Mattis",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/614s2iEcNzL._AC_UL200_SR200,200_.jpg",
+  },
+  {
+    id: 3,
+    author: "Mark Tappan",
+    title: "My Dog Mattis",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/614s2iEcNzL._AC_UL200_SR200,200_.jpg",
+  },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
+    </section>
+  );
+}
+
+const Book = ({ img, title, author }) => {
+  // attribute, eventHandler
+
+  // onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert("hello world");
+  };
+  const complexExample = (author) => {
+    console.log({ author });
+  };
+  return (
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
+      <img src={img} alt="" />
+      <h1 onClick={() => console.log({ title })}>{title}</h1>
+      <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        reference example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        more complex example
+      </button>
+    </article>
+  );
+};
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
