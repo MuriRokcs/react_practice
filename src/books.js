63366cb0f5ecc19a1e29892ebd75e8d912b1d50b
MuriRokcs <32195775+MuriRokcs@users.noreply.github.com>
2021-04-11 15:48:07 +0200

    imports and exports

---
+++ src/books.js
@@ -0,0 +1,23 @@
+export const books = [
+  {
+    id: 1,
+    author: "Amelia Hepworth",
+    title: "I Love You to the Moon and Back",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+  },
+  {
+    id: 2,
+    author: "Mark Tappan",
+    title: "My Dog Mattis",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/614s2iEcNzL._AC_UL200_SR200,200_.jpg",
+  },
+  {
+    id: 3,
+    author: "Mark Tappan",
+    title: "My Dog Mattis",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/614s2iEcNzL._AC_UL200_SR200,200_.jpg",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -3,29 +3,8 @@ import ReactDOM from "react-dom";
 
 // CSS
 import "./index.css";
-const books = [
-  {
-    id: 1,
-    author: "Amelia Hepworth",
-    title: "I Love You to the Moon and Back",
-    img:
-      "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
-  },
-  {
-    id: 2,
-    author: "Mark Tappan",
-    title: "My Dog Mattis",
-    img:
-      "https://images-na.ssl-images-amazon.com/images/I/614s2iEcNzL._AC_UL200_SR200,200_.jpg",
-  },
-  {
-    id: 3,
-    author: "Mark Tappan",
-    title: "My Dog Mattis",
-    img:
-      "https://images-na.ssl-images-amazon.com/images/I/614s2iEcNzL._AC_UL200_SR200,200_.jpg",
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 function BookList() {
   return (
@@ -36,37 +15,5 @@ function BookList() {
     </section>
   );
 }
-
-const Book = ({ img, title, author }) => {
-  // attribute, eventHandler
-
-  // onClick, onMouseOver
-  const clickHandler = (e) => {
-    console.log(e);
-    console.log(e.target);
-    alert("hello world");
-  };
-  const complexExample = (author) => {
-    console.log({ author });
-  };
-  return (
-    <article
-      className="book"
-      onMouseOver={() => {
-        console.log(title);
-      }}
-    >
-      <img src={img} alt="" />
-      <h1 onClick={() => console.log({ title })}>{title}</h1>
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>
-        reference example
-      </button>
-      <button type="button" onClick={() => complexExample(author)}>
-        more complex example
-      </button>
-    </article>
-  );
-};
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
